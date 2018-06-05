e999da3c61a31c022e2dd8267ae1cd33a93f9b24
braelynmuskwe <braemira1@icloud.com>
2018-06-05 22:03:41 +0000

    automatically backed up by learn

--- late_lunch.rb
+++ late_lunch.rb
@@ -1,13 +1,13 @@
 def fizzbuzz(i)
-  if i % 3 == 0
-    puts "fizz"
-    elsif i % 5 == 0
-    puts "buzz"
     elsif i % 5 == 0 && i % 3 == 0
-    puts "fizzbuzz"
+    puts "Fizzbuzz"
+  elsif i % 5 == 0
+    puts "Buzz"
+    elsif i % 5 == 0
+    puts "Buzz"
   else
     puts nil
 end
 end
 
-fizzbuzz(4)
+fizzbuzz(5)
